412425c737c969ce7523827bae1f8dc20d571688
VishnuSangammanavar <vishnusangammanavar25@gmail.com>
2024-02-06 16:37:32 +0530

    Update file

--- src/Components/WeatherInfo.js
+++ src/Components/WeatherInfo.js
@@ -5,7 +5,6 @@ import WaterDropIcon from '@mui/icons-material/WaterDrop';
 import AirIcon from '@mui/icons-material/Air';
 
 const WeatherInfo = ({search, inputEvent, weather, main, city, hour, peroid}) => {
-
   return (
     <div className="col-lg-3 col-md-4 weather-info" style={ {backgroundImage: `${(hour>5) && (peroid === 'PM')? "url('./img/night.jpg')": "url('./img/day.jpg')"}`} }>
       <div className="content">
